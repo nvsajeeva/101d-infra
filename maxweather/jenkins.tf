c6a24f5da8ebee995c50c4368f4174259d43c4a0
slakmal <slakmal@wiley.com>
2022-03-19 10:04:28 +0530

    Jenkins ec2

--- maxweather/jenkins.tf
+++ maxweather/jenkins.tf
@@ -18,3 +18,18 @@ module "ec2_instance" {
   }
 }
 */
+
+resource "aws_instance" "jenkins" {
+  subnet_id               = data.aws_subnets.example.ids[0]
+  ami                     = "ami-0c02fb55956c7d316"
+  key_name                = module.aws-keypair.key_name
+  instance_type           = "t2.micro"
+  vpc_security_group_ids      = [aws_default_security_group.default.id]
+  disable_api_termination = false
+  user_data               = null
+  tags = {
+    Name   = "101d-jenkins"
+    Owner = "101d-devops"
+  }
+
+}
