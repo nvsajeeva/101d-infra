6ef0c83c44893e1aeddb1bb1de0c881238dce5b7
slakmal <slakmal@wiley.com>
2022-03-19 19:07:47 +0530

    Maven and git installation with user_data

--- maxweather/install_jenkins.sh
+++ maxweather/install_jenkins.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo yum update –y
+sudo yum install git -y
+sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
