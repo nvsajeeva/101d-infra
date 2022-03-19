1d0a959ac5e774fed6f26beb4fdc39c26abf8561
slakmal <slakmal@wiley.com>
2022-03-19 17:55:10 +0530

    install jenkins via user_data

--- maxweather/jenkins.tf
+++ maxweather/jenkins.tf
@@ -1,23 +1,3 @@
-/*
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "101d-jenkins"
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-*/
 
 resource "aws_instance" "jenkins" {
   subnet_id               = data.aws_subnets.example.ids[0]
@@ -26,7 +6,7 @@ resource "aws_instance" "jenkins" {
   instance_type           = "t2.micro"
   vpc_security_group_ids      = [aws_default_security_group.default.id]
   disable_api_termination = false
-  user_data               = null
+  user_data               = "${file("install_jenkins.sh")}"
   tags = {
     Name   = "101d-jenkins"
     Owner = "101d-devops"
--- maxweather/security-group.tf
+++ maxweather/security-group.tf
@@ -16,6 +16,22 @@ resource "aws_default_security_group" "default" {
       protocol = -1
       self = true
   }
+ ingress {
+    description      = "traffic from outside"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   ingress {
+    description      = "traffic from outside"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
